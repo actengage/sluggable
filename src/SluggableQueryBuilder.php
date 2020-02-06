638258035d8143fa46ace9f9c9da461d4b4f6b48
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro-2.local>
2020-02-05 21:23:26 -0700

    Fixed issue with searching keys by array

--- src/SluggableQueryBuilder.php
+++ src/SluggableQueryBuilder.php
@@ -12,26 +12,16 @@ class SluggableQueryBuilder extends Builder
         if(is_numeric($id)) {
             return parent::whereKey($id);
         }
-        
-        if($id instanceof Arrayable) {
-            $id = $id->toArray();
-        }
 
-        if(is_array($id)) {
-            $values = array_map(function($id) {
+        $ids = collect($id instanceof Arrayable ? $id->toArray() : $id);
+
+        return $this->query->where(function($query) use ($ids) {
+            $slugs = $ids->map(function($id) {
                 return $this->model->slugify($id);
-            }, $id);
+            });
 
-            array_push($values, $id);
-
-            $this->query->whereIn($this->model->getSlugAttributeName(), array_unique($values));
-
-            return $this;
-        }
-
-        return $this->where(function($query) use ($id) {
-            $query->orWhere($this->model->getSlugAttributeName(), '=', $id);
-            $query->orWhere($this->model->getSlugAttributeName(), '=', $this->model->slugify($id));
+            $query->orWhereIn($this->model->getKeyName(), $ids->unique());
+            $query->orWhereIn($this->model->getSlugAttributeName(), $slugs->unique());
         });
     }
 
